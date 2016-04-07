64fda541165b80d5d909dd0aec0f3b7ebedeeef1
Julio Rodriguez <Julio@Julios-MacBook-Pro.local>
2016-04-07 10:54:03 -0400

    Icons working

--- JS/sript.js
+++ JS/sript.js
@@ -11,4 +11,56 @@ $(document).ready( function() {
     }, {
         offset: '100px;'
     });
+    
+        /*Navigation Scroll*/
+
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    
+    /*Animations on scroll*/
+    $('.js--waypoint-1').waypoint(function(direction){
+        $('.js--waypoint-1').addClass('animated fadeIn');
+        
+    }, {
+        offset: "50%"
+    });
+
+
+    $('.js--waypoint-2').waypoint(function(direction){
+        $('.js--waypoint-2').addClass('animated fadeInUp');
+        
+    }, {
+        offset: "50%"
+    });
+    
+
+    $('.js--waypoint-3').waypoint(function(direction){
+        $('.js--waypoint-3').addClass('animated fadeIn');
+        
+    }, {
+        offset: "50%"
+    });
+
+    $('.js--waypoint-4').waypoint(function(direction){
+        $('.js--waypoint-4').addClass('animated pulse');
+        
+    }, {
+        offset: "50%"
+    });
+
 });
+
+
+
